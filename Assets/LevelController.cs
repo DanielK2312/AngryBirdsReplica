a8b45f05ac9f20a041fad015d4b8eae26f170ee1
DanielK2312 <kosmindaniel@gmail.com>
2020-09-13 19:31:35 -0400

    Adding Levels and Particle System

---
+++ Assets/LevelController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEditor.PackageManager;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelController : MonoBehaviour
+{
+    private static int _nextLevelIndex = 1; // Look up why this is static
+    private Enemy[] _enemies;
+
+    private void OnEnable()
+    {
+        _enemies = FindObjectsOfType<Enemy>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (Enemy enemy in _enemies)
+        {
+            if (enemy != null)
+            {
+                return; // If any enemies are alive, we want to go back and restart 
+            }
+        }
+
+        Debug.Log("You Killed all Enemies");
+
+        _nextLevelIndex++; // One all enemies are killed, next level index increases by one 
+        string nextLevelName = "Level" + _nextLevelIndex;
+        SceneManager.LoadScene(nextLevelName);
+    }
+}
